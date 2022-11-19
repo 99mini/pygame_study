0aa2212304aadd31111f535757b7c980fa67bb3f
99mini <itman390@gmail.com>
2022-11-19 11:39:52 +0900

    display pygame

---
+++ test.py
@@ -0,0 +1,31 @@
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+pygame.init()
+pygame.display.set_caption('pygame')
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+
+clock = pygame.time.Clock()
+
+done = False
+while not done:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            done = True
+    
+    screen.fill(WHITE)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
